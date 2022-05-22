62c958d77ef0a1b12cb4593f48f6d91945acf6b7
unknown <mudamudiberkelas@gmail.com>
2022-05-23 05:44:31 +0700

    appcenter

--- android/app/src/main/java/com/inflationcalc/MainApplication.java
+++ android/app/src/main/java/com/inflationcalc/MainApplication.java
@@ -1,4 +1,4 @@
-package com.inflationcalc;
+package com.CICDReactNative;
 
 import android.app.Application;
 import android.util.Log;
--- ios/InflationCalcTests/InflationCalcTests.m
+++ ios/InflationCalcTests/InflationCalcTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface InflationCalcTests : XCTestCase
+@interface CICDReactNativeTests : XCTestCase
 
 @end
 
-@implementation InflationCalcTests
+@implementation CICDReactNativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
